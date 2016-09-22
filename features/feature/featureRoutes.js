523af6c463d9ffcc916ac0659eb34e3231ef8522
Brian Hudson <brianjasonhudson@gmail.com>
2016-09-22 13:38:26 -0500

    "Added get endpoints to route"

--- features/feature/featureRoutes.js
+++ features/feature/featureRoutes.js
@@ -1,4 +1,9 @@
 const featureCtrl = require("./featureCtrl");
 module.exports = app => {
-  app.get("url", getFunc);
+  app.get("/api/name", getName);
+  app.get("/api/location", getLocation);
+  app.get("/api/occupations", getOccupations);
+  app.get("/api/occupations/latest", getLatestOccupation);
+  app.get("/api/getHobbies", getHobbies);
+  app.get("/api/hobbies/:type", getHobbyByType);
 }
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const {json} = require("body-parser");
+const middleware = require("./middleware/middleware")
 
 const app = express();
 const port = 5000;
 app.use(json());
+app.use(middleware.addHeaders);
 
 const featureRoutes = require ('./features/feature/featureRoutes.js');
 featureRoutes(app);
