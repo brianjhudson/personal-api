d4acb40c73f1504ae0dfb79c3866480d1d25308b
Brian Hudson <brianjasonhudson@gmail.com>
2016-09-22 13:55:15 -0500

    "Added data and getName function"

--- features/feature/featureCtrl.js
+++ features/feature/featureCtrl.js
@@ -1,6 +1,6 @@
 module.exports = {
   getName(req, res) {
-        
+    return res.status(200).json(me.name)
   },
   getLocation(req, res) {
 
@@ -17,4 +17,16 @@ module.exports = {
   getHobbyByType(req, res) {
 
   }
+}
+
+const me = {
+  name: {"name": "Brian Hudson"},
+  location: {"location": "Dallas"},
+  occupation: {"occupations": ["Retail Sales Clerk", "Legal Assistant", "Teacher"]},
+  hobbies: {"hobbies": [
+    {"name": "Reading", "type": "current"},
+    {"name": "Traveling", "type": "past"},
+    {"name": "Hiking", "type": "past"},
+    {"name": "Tennis", "type": "past"}
+  ]}
 }
